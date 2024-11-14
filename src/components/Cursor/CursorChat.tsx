774cf0591e87cdf3bd85bb3534d0cf18c2fe1e81
lvy <upwardsrwr@gmail.com>
2024-11-15 00:33:47 +0800

    feat: add chat cursor

--- src/components/Cursor/CursorChat.tsx
+++ src/components/Cursor/CursorChat.tsx
@@ -1,4 +1,28 @@
-const CursorsChat = () => {
-  return <div className="live-cursors">CursorsChat</div>
+import { CursorMode, type CursorChatProps } from '@/types/type'
+import CursorSvg from '../../../public/assets/CursorSVG'
+
+const CursorsChat = ({
+  cursor,
+  cursorState,
+  setCursorState,
+  updateMyPresence
+}: CursorChatProps) => {
+  return (
+    <div
+      className="absolute top-0 left-0"
+      style={{
+        transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`
+      }}
+    >
+      {cursorState.mode === CursorMode.Chat && (
+        <>
+          <CursorSvg color={'#000'} />
+          <div className="absolute top-5 left-2  bg-blue-500 rounded-full px-4 py-2 text-white text-sm leading-relaxed">
+            {cursorState.previousMessage && <div>{cursorState.previousMessage}</div>}
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
 export default CursorsChat
--- src/components/Live/index.tsx
+++ src/components/Live/index.tsx
@@ -1,10 +1,16 @@
 import LiveCursors from '@/components/Cursor/LiveCursors'
 import { useOthers, useMyPresence } from '@liveblocks/react'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
+import CursorChat from '../Cursor/CursorChat'
+import { CursorMode } from '@/types/type'
 
 const Live = () => {
   const others = useOthers()
   const [{ cursor }, updateMyPresence] = useMyPresence() as any
+
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden
+  })
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault()
@@ -15,6 +21,7 @@ const Live = () => {
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
     event.preventDefault()
+    setCursorState({ mode: CursorMode.Hidden })
     updateMyPresence({ cursor: null, message: null })
   }, [])
 
@@ -30,6 +37,14 @@ const Live = () => {
       className="w-full h-[100vh] bg-green-700 flex justify-center items-center text-center"
     >
       dsfdsfdsfds
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
       <LiveCursors others={others} />
     </div>
   )
